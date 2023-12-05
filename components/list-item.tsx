fb4919c65f80f30fd3f0bb95ddc7f9d03abc3699
dobzacode <corentin.kittel@gmail.com>
2023-12-05 13:31:53 +0100

    landing page done

---
+++ components/list-item.tsx
@@ -0,0 +1,12 @@
+import Icon from '@mdi/react';
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function ListItem ({path, children, href}: {path: string; children: string; href: string}) {
+    return (<li  className='border-primary1 glassmorphism relative cursor-pointer hover:scale-110 sub-heading duration-medium  max-w-[50rem] w-full border rounded-large text-primary1 p-small'> 
+        <Link href={href} className='h-full w-full'>
+            {children !== "Deezer" ? <Icon className='left-extra-small absolute -translate-y-1/2 inset-y-1/2' path={path}  size={3}/> : <Image className='left-[14px] absolute -translate-y-1/2 inset-y-1/2' width={34} height={34} src={path} alt={`${children} logo`}></Image>}
+            <p className='text-center'>{children}</p>
+        </Link>
+    </li>)
+}

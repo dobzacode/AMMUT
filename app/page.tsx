2931a40e0fd348da58f51a8e9782e210bef11d77
dobzacode <corentin.kittel@gmail.com>
2023-12-05 13:50:36 +0100

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import ListItem from "@/components/list-item";
 import social from "@/components/social";
-import { dynamicBlurDataUrl } from "@/lib/utils";
 import Image from "next/image";
 
 
@@ -9,14 +8,14 @@ import Image from "next/image";
 
 export  default async function Home() {
 
-  const blurImage = await dynamicBlurDataUrl("/DSC07907-2.jpg")
+  // const blurImage = await dynamicBlurDataUrl("/DSC07907-2.jpg")
 
   return (
     <main className="flex min-h-screen gap-small relative flex-col items-center justify-center px-small">
       
       <ul className="w-full flex flex-col gap-medium items-center relative z-50">
         {social.map(({name, href, path}) => {return <ListItem key={name} path={path} href={href} >{name}</ListItem>})}</ul>
-        <Image fill src={"/DSC07907-2.jpg"} placeholder='blur' blurDataURL={blurImage}  priority={true} alt="Ammut picture" className="object-cover object-[center_15%]"></Image>
+        <Image fill src={"/DSC07907-2.jpg"} placeholder='blur' blurDataURL={"/DSC07907-2.jpg"}  priority={true} alt="Ammut picture" className="object-cover object-[center_15%]"></Image>
         
      
     </main>

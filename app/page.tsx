6b83623739383f8f542bd5f640848cbd1bde2491
dobzacode <corentin.kittel@gmail.com>
2023-12-05 13:48:36 +0100

    clean done

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,9 @@
 import ListItem from "@/components/list-item";
+import social from "@/components/social";
 import { dynamicBlurDataUrl } from "@/lib/utils";
-import { mdiInstagram, mdiSoundcloud, mdiSpotify } from "@mdi/js";
 import Image from "next/image";
 
-const social = [
-  {
-    name: "Instagram",
-    path: mdiInstagram,
-    href: "https://www.instagram.com/ammut_music/",
-  },
-  {
-    name: "Spotify",
-    path: mdiSpotify,
-    href: "https://open.spotify.com/intl-fr/artist/5abcZtaPYTfNSI0c42JQft",
-  },
-  {
-    name: "Soundcloud",
-    path: mdiSoundcloud,
-    href: "https://soundcloud.com/ammut-3000"
-  }, 
-  {
-    name: "Deezer",
-    path: "/deezer.png",
-    href: "https://www.deezer.com/fr/artist/7708772"
-  }
-]
+
 
 
 
@@ -39,12 +18,7 @@ export  default async function Home() {
         {social.map(({name, href, path}) => {return <ListItem key={name} path={path} href={href} >{name}</ListItem>})}</ul>
         <Image fill src={"/DSC07907-2.jpg"} placeholder='blur' blurDataURL={blurImage}  priority={true} alt="Ammut picture" className="object-cover object-[center_15%]"></Image>
         
-          
-        
-        <video autoPlay loop muted className="background-video">
-        <source src="8mm.mov" type="video/quicktime" />
-        Votre navigateur ne supporte pas la lecture de la vidéo.
-      </video>
+     
     </main>
   )
 }
---
+++ components/social.ts
@@ -0,0 +1,26 @@
+import { mdiInstagram, mdiSoundcloud, mdiSpotify } from "@mdi/js"
+
+const social = [
+    {
+      name: "Instagram",
+      path: mdiInstagram,
+      href: "https://www.instagram.com/ammut_music/",
+    },
+    {
+      name: "Spotify",
+      path: mdiSpotify,
+      href: "https://open.spotify.com/intl-fr/artist/5abcZtaPYTfNSI0c42JQft",
+    },
+    {
+      name: "Soundcloud",
+      path: mdiSoundcloud,
+      href: "https://soundcloud.com/ammut-3000"
+    }, 
+    {
+      name: "Deezer",
+      path: "/deezer.png",
+      href: "https://www.deezer.com/fr/artist/7708772"
+    }
+  ]
+
+  export default social

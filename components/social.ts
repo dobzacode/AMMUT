2317db2087f33b2d3f3318f9acc65b710307becd
Corentin Kittel <dobzadub@gmail.com>
2024-03-04 20:03:11 +0100

    add come to hell with me

--- components/social.ts
+++ components/social.ts
@@ -1,32 +1,39 @@
-import { mdiInstagram, mdiSoundcloud, mdiSpotify } from "@mdi/js"
+import { mdiInstagram, mdiSoundcloud, mdiSpotify } from "@mdi/js";
 
 const social = [
-  { name: "Me Matt & Carl ",
-  path: "/me_matt_and_carl.jpg",
-  href: "https://soundcloud.com/ammut-3000/me-matt-carl-free-download",
-  isNotIcon: true,
-},
-    {
-      name: "Instagram",
-      path: mdiInstagram,
-      href: "https://www.instagram.com/ammut_music/",
-    },
-    {
-      name: "Spotify",
-      path: mdiSpotify,
-      href: "https://open.spotify.com/intl-fr/artist/5abcZtaPYTfNSI0c42JQft",
-    },
-    {
-      name: "Soundcloud",
-      path: mdiSoundcloud,
-      href: "https://soundcloud.com/ammut-3000"
-    }, 
-    {
-      name: "Deezer",
-      path: "/deezer.png",
-      href: "https://www.deezer.com/fr/artist/7708772",
-      isNotIcon: true,
-    }
-  ]
+  {
+    name: "Come To Hell With Me ",
+    path: "/come_to_hell_with_me.jpg",
+    href: "https://soundcloud.com/ammut-3000/come-to-hell-with-me-free-download",
+    isNotIcon: true,
+  },
+  {
+    name: "Me Matt & Carl ",
+    path: "/me_matt_and_carl.jpg",
+    href: "https://soundcloud.com/ammut-3000/me-matt-carl-free-download",
+    isNotIcon: true,
+  },
+  {
+    name: "Instagram",
+    path: mdiInstagram,
+    href: "https://www.instagram.com/ammut_music/",
+  },
+  {
+    name: "Spotify",
+    path: mdiSpotify,
+    href: "https://open.spotify.com/intl-fr/artist/5abcZtaPYTfNSI0c42JQft",
+  },
+  {
+    name: "Soundcloud",
+    path: mdiSoundcloud,
+    href: "https://soundcloud.com/ammut-3000",
+  },
+  {
+    name: "Deezer",
+    path: "/deezer.png",
+    href: "https://www.deezer.com/fr/artist/7708772",
+    isNotIcon: true,
+  },
+];
 
-  export default social
+export default social;
